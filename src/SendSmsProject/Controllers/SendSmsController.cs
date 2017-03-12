234fb788a204599111738fee31db5638add7721e
Marty <martyletters@gmail.com>
2017-03-12 13:02:27 +0000

    Added NewMessage Action Method

--- src/SendSmsProject/Controllers/SendSmsController.cs
+++ src/SendSmsProject/Controllers/SendSmsController.cs
@@ -6,7 +6,11 @@ using System.Web.Mvc;
 
 namespace SendSmsProject.Controllers
 {
-    public class SendSmsController  :Controller
+    public class SendSmsController : Controller
     {
+        public string NewMessage()
+        {
+            return "Hello from the Send sms controller";
+        }
     }
 }
