b3f529f31e213628186b0381baa5fa5ceea4becd
Marty <martyletters@gmail.com>
2017-03-31 16:30:04 +0100

    Added DB class

---
+++ src/SendSmsProject/Models/List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendSmsProject.Models
+{
+    interface iDb
+    {
+        List<Person> GetPeople();
+        Person GetPerson(int findid);
+        void CreatePerson(Person p);
+    }
+
+    public class MockDB : iDb // we have an inteface here
+    {
+        List<Person> people;
+
+        public MockDB() // we are faking a database here
+        {
+            people = new List<Person>();
+            people.Add(new Person() { Id = 1,Forename = "Ginette", Surename = "Shaeffer", AreaCode = 087, PhoneNumber = 2084057});
+            people.Add(new Person() { Id = 1, Forename = "Marceline", Surename = "Sprow", AreaCode = 088, PhoneNumber = 2084058 });
+            people.Add(new Person() { Id = 1, Forename = "Emmitt", Surename = "Holtkamp", AreaCode = 089, PhoneNumber = 2084059 });
+            people.Add(new Person() { Id = 1, Forename = "Drew ", Surename = "Craghead", AreaCode = 083, PhoneNumber = 2084060 });
+            people.Add(new Person() { Id = 1, Forename = "Mary ", Surename = "Schumacher", AreaCode = 082, PhoneNumber = 2084061 });
+            people.Add(new Person() { Id = 1, Forename = "Eladia ", Surename = "Shute", AreaCode = 084, PhoneNumber = 2084062 });
+            people.Add(new Person() { Id = 1, Forename = "Nicol ", Surename = "Skidmore", AreaCode = 085, PhoneNumber = 2084063 });
+            people.Add(new Person() { Id = 1, Forename = "Aleta ", Surename = "Roberge", AreaCode = 087, PhoneNumber = 2084064 });
+            people.Add(new Person() { Id = 1, Forename = "Almeta", Surename = "Bonomo", AreaCode = 089, PhoneNumber = 2084065 });
+            people.Add(new Person() { Id = 1, Forename = "Ginette", Surename = "Lish", AreaCode = 088, PhoneNumber = 2084066 });
+        }
+        public List<Person> GetPeople()
+        {
+            return people;
+        }
+
+        public Person GetPerson(int findid)
+        {
+            return people.Where(p => p.Id == findid).FirstOrDefault(); //returns the first person with findID 
+
+        }
+
+        public void CreatePerson(Person p)
+        {
+            people.Add(p);
+        }
+
+
+    }
+}
---
+++ src/SendSmsProject/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendSmsProject.Models
+{
+    public class Person
+    {
+        public int Id { get; set; } // we need id for the database
+        
+        public string Forename { get; set; }
+        public string Surename { get; set; }
+        public int AreaCode { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
