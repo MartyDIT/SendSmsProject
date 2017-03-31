29f22279b9cdfa289c102233280dfddb2e45dc8d
Marty <martyletters@gmail.com>
2017-03-31 19:14:39 +0100

    Added send new sms from the contact list

--- src/SendSmsProject/Controllers/ContactsController.cs
+++ src/SendSmsProject/Controllers/ContactsController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SendSmsProject.Models;
 
 namespace SendSmsProject.Controllers
 {
     public class ContactsController : Controller
     {
+        static private iDb db;
+
+        public ContactsController()
+        {
+            db = new MockDB();
+        }
+
         // GET: Contacts
         public ActionResult Index()
+        {
+           
+            var people = db.GetPeople();
+            return View(people);
+        }
+
+        public ActionResult SendText()
         {
             return View();
         }
